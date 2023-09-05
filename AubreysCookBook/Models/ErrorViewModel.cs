e5507a15e19c6ca37fb0eef887ab24897caa72d1
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-09-05 12:33:24 -0400

    Add project files.

---
+++ AubreysCookBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AubreysCookBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
